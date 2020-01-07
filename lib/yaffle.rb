b532af6e402027de65c0316cefb664bfd717b148
lengcb <649569822@qq.com>
2020-01-07 16:11:20 +0800

    second commit

---
+++ lib/yaffle.rb
@@ -0,0 +1,8 @@
+
+# yaffle/lib/yaffle.rb
+
+require 'yaffle/core_ext'
+require 'yaffle/acts_as_yaffle'
+
+module Yaffle
+end
---
+++ lib/yaffle/acts_as_yaffle.rb
@@ -0,0 +1,25 @@
+module Yaffle
+  module ActsAsYaffle
+    extend ActiveSupport::Concern
+
+    included do
+    end
+
+    module ClassMethods
+      def acts_as_yaffle(options = {})
+        # your code will go here
+        cattr_accessor :yaffle_text_field
+        self.yaffle_text_field = (options[:yaffle_text_field] || :last_squawk).to_s
+
+        include Yaffle::ActsAsYaffle::LocalInstanceMethods
+      end
+    end
+
+    module LocalInstanceMethods
+      def squawk(string)
+      	p self.class.yaffle_text_field
+        write_attribute(self.class.yaffle_text_field, string.to_squawk)
+      end
+    end
+  end
+end
